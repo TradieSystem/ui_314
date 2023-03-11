26d16b2c4f7df952d729b2640b178dc0de7c865d
AlexA0595 <117964800+AlexA0595@users.noreply.github.com>
2023-03-11 19:42:58 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import './App.css';
 import {NavigationContextContextProvider} from "./Contexts/NavigationContext";
 import {HomePage} from "./Pages/HomePage";
 import {RoutesEnum} from "./Routes";
-<<<<<<< Updated upstream
 import {MemoryRouter, Route, Routes} from "react-router-dom";
-=======
 import Login from "./Login/Login";
 import {HashRouter, Route, Routes} from "react-router-dom";
->>>>>>> Stashed changes
 import PageContainer from "./Components/PageContainer/PageContainer";
 function App() {
     return (
