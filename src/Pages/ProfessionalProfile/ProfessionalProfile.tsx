9ee62998baa81c51c8246cce62af22a4f19064af
Veronica <veronica.nashed@gmail.com>
2023-03-31 22:57:22 +1100

    Professional Profile PR
    1. updated the package-lock.json file to match the package.json
    2. updated styling (centered profile pic/name/rating, rounded edges on table, removed box shadow from table, reduced gap between header content and table)
    3. added unique identifier in rows to prevent console error
    4. code formatting/removed unused imports

--- src/Pages/ProfessionalProfile/ProfessionalProfile.tsx
+++ src/Pages/ProfessionalProfile/ProfessionalProfile.tsx
@@ -1,68 +1,54 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
-import { useAuthContext } from '../../Contexts/AuthContext';
-import { UserType } from '../../Types/Account';
+import {useAuthContext} from '../../Contexts/AuthContext';
 import {
     Avatar,
-    Badge,
-    IconButton,
     Stack,
     styled,
-    TableContainer,
     Table,
     TableBody,
     TableCell,
+    tableCellClasses,
+    TableContainer,
     TableHead,
     TableRow,
-    TableSortLabel,
-    tableCellClasses,
 } from "@mui/material";
-import styles from 'ClientRequestHistory.module.css';
-import Paper from '@mui/material/Paper';
-import { Edit } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
-import {useState} from "react";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+const StyledTableCell = styled(TableCell)(({theme}) => ({
     [`&.${tableCellClasses.head}`]: {
         backgroundColor: "#d3733c",
         color: theme.palette.common.white,
-
     },
     [`&.${tableCellClasses.body}`]: {
         fontSize: 16,
     },
+    borderBottom: 0
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
+const StyledTableRow = styled(TableRow)(({theme}) => ({
     '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-
+        backgroundColor: "#e5e5e5",
     },
-    // hide last border
+    backgroundColor: "white",
     '&:last-child td:first-of-type': {
-        borderBottomLeftRadius: 10,
+        borderBottomLeftRadius: 12,
     },
     '&:last-child td:last-child': {
-        borderBottomRightRadius: 10,
+        borderBottomRightRadius: 12,
     },
-
 }));
 
 
 function createData(
     ServiceDate: string,
     ServiceType: string,
-     Review: string,
+    Review: string,
 ) {
     return {
         ServiceDate,
         ServiceType,
         Review
-
-
     };
 }
 
@@ -83,75 +69,92 @@ const rows = [
 //<Edit />
 //</IconButton>
 
-export const  ProfessionalProfile = () => {
+export const ProfessionalProfile = () => {
     const {user} = useAuthContext();
     const [value, setValue] = React.useState<number | null>(2);
-    const navigate = useNavigate();
-    if(user){
-        return (
 
-                <Box>
-                    <Box>
-                        &nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        &nbsp;&nbsp;&nbsp;
-                        <Badge>
-                            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" sx={{
+    if (user) {
+        return (
+            <Box>
+                <Box sx={{
+                    display: "grid",
+                    justifyContent: "center"
+                }}>
+                    <div style={{justifyContent: "center", display: "flex"}}>
+                        <Avatar
+                            alt="Travis Howard"
+                            src="/static/images/avatar/2.jpg"
+                            sx={{
                                 width: 90,
                                 height: 90,
                             }}
-                            />
-                            &nbsp;&nbsp;
-                            <Box>
-                                &nbsp;&nbsp;&nbsp;
-                                <h1>{user.firstname} {user.lastname}</h1>
-                                <Rating name="read-only" value={value} readOnly/>
-                            </Box>
-                        </Badge>
-                    </Box>
-                    <Stack
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="space-between"
-                        sx={{my: 8}}
-                    >
-                    </Stack>
-                    <Box>
-                        <TableContainer component={Paper}>
-                            <Table sx={{
-                                minWidth: 600, minHeight: 500, orderRadius: "25px",
-                                padding: "20px"
-                            }} aria-label="customized table">
-                                <TableHead>
-                                    <TableRow>
-                                        <StyledTableCell style={{fontSize: "18px"}} align="center">Service
-                                            Date</StyledTableCell>
-                                        <StyledTableCell style={{fontSize: "18px"}} align="center">Service
-                                            Type</StyledTableCell>
-                                        <StyledTableCell style={{fontSize: "18px"}}
-                                                         align="center">Review</StyledTableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {rows.map((row) => (
-                                        <StyledTableRow key={row.ServiceDate}>
-                                            <StyledTableCell component="th" scope="row" align="center">
-                                                {row.ServiceDate}
-                                            </StyledTableCell>
-                                            <StyledTableCell align="center">{row.ServiceType}</StyledTableCell>
-                                            <StyledTableCell align="center">{row.Review}</StyledTableCell>
-                                        </StyledTableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
+                        />
+                    </div>
+                    <Box style={{justifyContent: "center", display: "grid"}}>
+                        <h1>{user.firstname} {user.lastname}</h1>
+                        <Rating name="read-only" value={value} readOnly sx={{marginLeft: 2}}/>
                     </Box>
                 </Box>
-            );
-        }
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    sx={{my: 2}}
+                >
+                </Stack>
+                <Box>
+                    <TableContainer>
+                        <Table
+                            sx={{
+                                minWidth: 600,
+                                minHeight: 500,
+                                backgroundColor: "#D8CECD"
+                            }}
+                            aria-label="customized table"
+                        >
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell
+                                        style={{fontSize: "18px"}}
+                                        align="center"
+                                        sx={{borderRadius: "12px 0 0 0", background: "inherit"}}
+                                    >
+                                        Service Date
+                                    </StyledTableCell>
+                                    <StyledTableCell
+                                        style={{fontSize: "18px"}}
+                                        align="center"
+                                    >
+                                        Service Type
+                                    </StyledTableCell>
+                                    <StyledTableCell
+                                        style={{fontSize: "18px"}}
+                                        align="center"
+                                        sx={{borderRadius: "0 12px 0 0", background: "inherit"}}
+                                    >
+                                        Review
+                                    </StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row, id) => (
+                                    <StyledTableRow
+                                        key={id}
+                                    >
+                                        <StyledTableCell scope="row" align="center">
+                                            {row.ServiceDate}
+                                        </StyledTableCell>
+                                        <StyledTableCell align="center">{row.ServiceType}</StyledTableCell>
+                                        <StyledTableCell align="center">{row.Review}</StyledTableCell>
+                                    </StyledTableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
+            </Box>
+        );
     }
+}
 
 export default ProfessionalProfile;
