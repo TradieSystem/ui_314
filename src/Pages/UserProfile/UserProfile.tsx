e68c10d18e4a17e10f0406acd4f2fce795e42f43
Greg950-glitch <117964800+AlexA0595@users.noreply.github.com>
2023-04-02 17:21:57 +1000

    Update To User Profile
    Update To User Profile

--- src/Pages/UserProfile/UserProfile.tsx
+++ src/Pages/UserProfile/UserProfile.tsx
@@ -2,44 +2,81 @@ import React from "react";
 import {useNavigate} from 'react-router-dom';
 import {useAuthContext} from "../../Contexts/AuthContext";
 import PageContainer from "../../Components/PageContainer/PageContainer";
-import {Box} from "@mui/material";
+import {Box, Stack} from "@mui/material";
 import {ThemedButton} from "../../Components/Button/ThemedButton";
 import {RoutesEnum} from "../../Routes";
+import  {ManageAccounts} from "@mui/icons-material";
+import  {AccountBoxSharp} from "@mui/icons-material";
+
 
 const UserProfile = () => {
     const navigate = useNavigate();
     const {user} = useAuthContext();
     if (user) {
+
         return (
             <PageContainer title={''}>
                 <Box
                     sx={{
                         display: "flex",
                         flexDirection: "column",
-                        gap: 3,
+                        gap: 2,
                     }}>
-                    <div style={{textAlign: "center"}}>
-                        <h1>User Page</h1>
+
+                    <Stack sx={{
+                        justifyContent: "center"
+                        , display: "grid"
+                    }}>
+                        <AccountBoxSharp style={{ width:"150px",height:"150px"}}/>
+                        <h1 style={{ fontSize: "30px"}}>User Page</h1>
+                    </Stack>
+
+                    <div style={{textAlign: "center",
+                        background: "rgb(220,220,220)",
+                        borderRadius: "20px",
+                        border: "2px solid #DB5B13",
+                        height: "70px",
+                        width: "360px"}}>
+                        <p style={{fontSize: "20px"}}>First & Last Name: {user.firstname} {user.lastname}</p>
                     </div>
-                    <div style={{textAlign: "center"}}>
-                        <p style={{fontSize: "20px"}}>First Name: {user.firstname}</p>
-                    </div>
-                    <div style={{textAlign: "center"}}>
-                        <p style={{fontSize: "20px"}}>Last Name: {user.lastname} </p>
-                    </div>
-                    <div style={{textAlign: "center"}}>
+                    <div style={{textAlign: "center",
+                        background: "rgb(220,220,220)",
+                        borderRadius: "20px",
+                        border: "2px solid #DB5B13",
+                        height: "70px",
+                        width: "360px"}}>
                         <p style={{fontSize: "20px"}}>Mobile: {user.mobile} </p>
                     </div>
-                    <div style={{textAlign: "center"}}>
+                    <div style={{textAlign: "center",
+                        background: "rgb(220,220,220)",
+                        borderRadius: "20px",
+                        border: "2px solid #DB5B13",
+                        height: "70px",
+                        width: "360px"}}>
                         <p style={{fontSize: "20px"}}>Street: {user.address.streetNumber && user.address.streetName} </p>
                     </div>
-                    <div style={{textAlign: "center"}}>
+                    <div style={{textAlign: "center",
+                        background: "rgb(220,220,220)",
+                        borderRadius: "20px",
+                        border: "2px solid #DB5B13",
+                        height: "70px",
+                        width: "360px"}}>
                         <p style={{fontSize: "20px"}}>Suburb: {user.address.suburb} </p>
                     </div>
-                    <div style={{textAlign: "center"}}>
+                    <div style={{textAlign: "center",
+                        background: "rgb(220,220,220)",
+                        borderRadius: "20px",
+                        border: "2px solid #DB5B13",
+                        height: "70px",
+                        width: "360px"}}>
                         <p style={{fontSize: "20px"}}>postcode: {user.address.postcode} </p>
                     </div>
-                    <div style={{textAlign: "center"}}>
+                    <div style={{textAlign: "center",
+                            background: "rgb(220,220,220)",
+                            borderRadius: "20px",
+                            border: "2px solid #DB5B13",
+                            height: "70px",
+                            width: "360px"}}>
                         <p style={{fontSize: "20px"}}>User Type: {user.usertype} </p>
                     </div>
                     <Box
@@ -55,7 +92,7 @@ const UserProfile = () => {
                             type="button"
                             onClick={() => navigate(`/${RoutesEnum.EDIT_PROFILE}`)}
                         >
-                            Edit Profile
+                            Edit Profile <ManageAccounts/>
                         </ThemedButton>
                     </Box>
                 </Box>
