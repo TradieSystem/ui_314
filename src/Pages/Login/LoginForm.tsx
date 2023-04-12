5efb87b7d9b038c97b35aed471138dbb6015f9db
Greg950-glitch <117964800+AlexA0595@users.noreply.github.com>
2023-04-12 22:17:18 +1000

    a
    a

--- src/Pages/Login/LoginForm.tsx
+++ src/Pages/Login/LoginForm.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, {useContext, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Form, FormikProvider, useFormik } from "formik";
 import * as Yup from "yup";
 import Loading from "../../Effects/loading";
 import {
+    Alert,
     Box,
     Checkbox,
     FormControlLabel,
@@ -18,15 +19,27 @@ import { motion } from "framer-motion";
 import { animate } from "../../Effects/Animations";
 import ThemedTextField from "../../Components/TextField/ThemedTextField";
 import {ThemedButton} from "../../Components/Button/ThemedButton";
-import {RoutesEnum} from "../../Routes";
+import {DEV_PATH, RoutesEnum} from "../../Routes";
+import axios from "axios";
+import { useAuthContext } from "../../Contexts/AuthContext";
+import { User } from "../../Types/User";
+import swal from 'sweetalert';
+import "./Swal.css";
+import { UserType } from "../../Types/Account";
+
+
 
 const LoginForm = () => {
 
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || "/";
-
     const [showPassword, setShowPassword] = useState(false);
+
+    const {setUser, setAuthToken,setRefreshToken}= useAuthContext();
+
+
+
 
     const LoginSchema = Yup.object().shape({
         email: Yup.string()
@@ -41,13 +54,51 @@ const LoginForm = () => {
             password: "",
             remember: true,
         },
-        validationSchema: LoginSchema,
-        onSubmit: () => {
-            setTimeout(() => {
-                navigate(from, { replace: true });
-            }, 2000);
-        },
-    });
+
+
+    validationSchema: LoginSchema,
+        onSubmit: ({email,password}) => {
+            axios.post((`${DEV_PATH}/user/login?email=${email}&password=${password}`), {
+                headers: {
+                    'content-type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+            })
+                .then((response) => {
+                    let userObject = response.data.user;
+                    if(response.data.user.professional === null && response.data.user.client !== null) {
+                        userObject.userType = UserType.CLIENT
+                    }
+                    if(response.data.user.client === null && response.data.user.professional !== null) {
+                        userObject.userType = UserType.PROFESSIONAL
+                    }
+                    if(response.data.user.client !== null && response.data.user.professional !== null) {
+                        userObject.userType = UserType.PROFESSIONAL
+                    }
+                    if (response.data.user) {
+                        try {
+                            setUser(response.data.user);
+                            setUser(userObject);
+                            setAuthToken(response.data.access_token);
+                            setRefreshToken(response.data.refresh_token);
+                            swal("Good job!", "You Have Signed In!", "success");
+                            navigate("/" + RoutesEnum.HOME)
+
+                        } catch (error) {
+                            //This error will appear if we receive a 200, with an object that isn't a user
+                            swal("Cant Sign in wrong email/password!")
+                        }
+                    } else {
+                        throw new Error();
+                    }
+                })
+                .catch(() => {
+                    swal("Cant Sign in wrong email/password!" );
+
+                });
+        }
+});
+
 
     const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } =
         formik;
@@ -79,6 +130,8 @@ const LoginForm = () => {
                             type="email"
                             label="Email Address"
                             {...getFieldProps("email")}
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
                             error={Boolean(touched.email && errors.email)}
                             helperText={touched.email && errors.email}
                         />
@@ -89,6 +142,8 @@ const LoginForm = () => {
                             type={showPassword ? "text" : "password"}
                             label="Password"
                             {...getFieldProps("password")}
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
                             error={Boolean(touched.password && errors.password)}
                             helperText={touched.password && errors.password}
                             InputProps={{
@@ -162,6 +217,3 @@ const LoginForm = () => {
 
 export default LoginForm;
 
-function AuthContextState(arg0: boolean) { //Do we need this?
-    throw new Error("Function not implemented.");
-}
