e8eb1b61f055858e94298e0f02f3e8a6b5cd2e57
Greg950-glitch <117964800+AlexA0595@users.noreply.github.com>
2023-04-18 20:32:39 +1000

    Nav Bar Updated
    Nav Bar Updated. Icons on the bottom

--- src/Components/SideNavigation/SideNavigation.tsx
+++ src/Components/SideNavigation/SideNavigation.tsx
@@ -3,14 +3,17 @@ import SideNavigationMenuItem from "./SideNavigationMenuItem/SideNavigationMenuI
 import {useNavigationContext} from "../../Contexts/NavigationContext";
 import styles from './SideNavigation.module.css';
 import {Drawer} from "@mui/material";
-import {ChevronLeft, ChevronRight} from "@mui/icons-material";
+import {AccountCircle, ChevronLeft, ChevronRight, Person} from "@mui/icons-material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import {useNavigate} from "react-router-dom";
 import {RoutesEnum} from "../../Routes";
+import {User} from "../../Types/User";
+import { UserType } from "../../Types/Account";
 
 export const SideNavigation = () => {
     const {isExpanded, setIsExpanded, sideNavigationMenuItems} = useNavigationContext();
     const navigate = useNavigate();
+    const user: User = JSON.parse(localStorage.getItem("user") || "{}") as User;
     return (
         <>
             <Drawer
@@ -41,6 +44,44 @@ export const SideNavigation = () => {
                         )
                     })
                 }
+
+                    {user.userType === UserType.PROFESSIONAL &&
+                        <div style={{padding:"0.5px ", marginTop:"780px"}}>
+                    <SideNavigationMenuItem
+                    icon={<Person/>}
+                    text={'Profile Page'}
+                    onClick={() => {
+                        navigate(`/${RoutesEnum.Pro_Profile}`);
+                    }}
+                    />
+                        <SideNavigationMenuItem
+                        icon={<AccountCircle />}
+                        text= {'Account Profile'}
+                        onClick={() => {
+                        navigate(`/${RoutesEnum.USER_MANAGEMENT}`);
+                    }}
+                        />
+                        <SideNavigationMenuItem
+                        icon={<LogoutIcon />}
+                        text={"Logout"}
+                        onClick={() => {
+                        localStorage.setItem("user", "");
+                        localStorage.setItem("access_token", "");
+                        localStorage.setItem("refresh_token", "");
+                        navigate(`/${RoutesEnum.LOGIN}`);
+                    }}
+                        />
+                        </div>
+                    }
+                {user.userType === UserType.CLIENT &&
+                    <div style={{padding:"0.5px ", marginTop:"800px"}}>
+                <SideNavigationMenuItem
+                    icon={<AccountCircle />}
+                    text= {'Account Profile'}
+                    onClick={() => {
+                        navigate(`/${RoutesEnum.USER_MANAGEMENT}`);
+                    }}
+                />
                 <SideNavigationMenuItem
                     icon={<LogoutIcon />}
                     text={"Logout"}
@@ -51,6 +92,8 @@ export const SideNavigation = () => {
                         navigate(`/${RoutesEnum.LOGIN}`);
                     }}
                 />
+            </div>
+                }
             </Drawer>
         </>
     )
