309db9aaae3ff8ff2f1ac4032aa34dfb7a2e80a4
Greg950-glitch <117964800+AlexA0595@users.noreply.github.com>
2023-04-26 22:35:11 +1000

    Updates to request
    Updates to request import {ServiceRequestReview} from "../../Types/ServiceRequest"     reviews: ServiceRequestReview[];

--- src/Pages/ProfessionalProfile/CarouselReview.tsx
+++ src/Pages/ProfessionalProfile/CarouselReview.tsx
@@ -1,60 +1,19 @@
 import React from 'react';
 import Slider from "react-slick";
 import {Grid, Typography} from "@mui/material";
- interface Review {
-    id: number;
-
-    service: string;
-
-    content: string;
-
-    rating: number;
-
-}
-
+import {ServiceRequestReview} from "../../Types/ServiceRequest"
 
 interface ReviewCardProps {
-    reviews: Array<Review>;
+    reviews: ServiceRequestReview[];
 }
-const reviews = [
-    {
-        id: 1,
-        service: "Plumbing",
-        content: "This is an amazing boiler that has been installed! I would highly recommend it.",
-        rating: 5
-    },
-    {
-        id: 2,
-        service: "Plumbing",
-        content: "I was pleasantly surprised by his work. It exceeded my expectations.",
-        rating: 4
-    },
-    {
-        id: 3,
-        service: "Plumbing",
-        content: "I have been using this plumber for a months now and I am very happy with him.",
-        rating: 4
-    },
-    {
-        id: 4,
-        service: "Plumbing",
-        content: "They install a new pipes for my house! It has made my life so much easier.",
-        rating: 5
-    },
-    {
-        id: 5,
-        service: "Plumbing",
-        content: "Plumbing job was okay. It works fine but I've had some issues with the toliet.",
-        rating: 3
-    },
-    {
-        id: 6,
-        service: "Plumbing",
-        content: "I wouldn't recommend this plumber. He doesn't work just drinks on the job",
-        rating: 2
-    }
-];
 
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+};
 const ReviewCard = ({ reviews }: ReviewCardProps) => {
     return (
         <div style={{ margin: "8px",
@@ -63,13 +22,13 @@ const ReviewCard = ({ reviews }: ReviewCardProps) => {
             padding:"50px",
             background: "#d9c8c6"}}>
             {reviews.map(review => (
-                <div key={review.id} style={{ marginBottom: "20px"}}>
+                <div key={review.requestID} style={{ marginBottom: "20px"}}>
                     <Grid container spacing={4} style={{justifyContent: "center", display: "flex",padding:"20px"}}>
                         <Grid item xs={3}>
-                            <Typography><b>Service:</b> {review.service}</Typography>
+                            <Typography><b>Service:</b> {review.requestID}</Typography>
                         </Grid>
                         <Grid item xs={5}>
-                            <Typography><b>Review:</b> {review.content}</Typography>
+                            <Typography><b>Review:</b> {review.review}</Typography>
                         </Grid>
                         <Grid item xs={3}>
                             <Typography><b>Rating:</b> {review.rating}/5</Typography>
@@ -80,14 +39,8 @@ const ReviewCard = ({ reviews }: ReviewCardProps) => {
         </div>
     );
 };
-const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-};
-const CarouselReview = () => {
+
+const CarouselReview = ({reviews}: ReviewCardProps) => {
     const groupedReviews = reviews.reduce((acc: any[], review, i) => {
         const groupIndex = Math.floor(i / 3);
         acc[groupIndex] = acc[groupIndex] || [];
--- src/Pages/ProfessionalProfile/ProfessionalProfile.tsx
+++ src/Pages/ProfessionalProfile/ProfessionalProfile.tsx
@@ -8,7 +8,38 @@ import {User} from "../../Types/User";
 import {motion} from "framer-motion";
 import {fadeInUp} from "../../Effects/Animations";
 import CarouselReview from './CarouselReview';
-
+const reviews = [
+    {
+        requestID: 1,
+        review: "This is an amazing boiler that has been installed! I would highly recommend it.",
+        rating: 5
+    },
+    {
+        requestID: 2,
+        review: "I was pleasantly surprised by his work. It exceeded my expectations.",
+        rating: 4
+    },
+    {
+        requestID: 3,
+        review: "I have been using this plumber for a months now and I am very happy with him.",
+        rating: 4
+    },
+    {
+        requestID: 4,
+        review: "They install a new pipes for my house! It has made my life so much easier.",
+        rating: 5
+    },
+    {
+        requestID: 5,
+        review: "Plumbing job was okay. It works fine but I've had some issues with the toliet.",
+        rating: 3
+    },
+    {
+        requestID: 6,
+        review: "I wouldn't recommend this plumber. He doesn't work just drinks on the job",
+        rating: 2
+    }
+];
 
 
 export const ProfessionalProfile = () => {
@@ -21,10 +52,6 @@ export const ProfessionalProfile = () => {
                 component={motion.div}
                 {...fadeInUp}
             >
-                <Box sx={{
-                    display: "grid",
-                    justifyContent: "center"
-                }}>
                     <div style={{justifyContent: "center", display: "flex"}}>
                         <Avatar
                             alt="Travis Howard"
@@ -39,12 +66,11 @@ export const ProfessionalProfile = () => {
                         <h1>{user.firstName} {user.lastName}</h1>
                         <center><Rating name="read-only" value={value} readOnly/></center>
                     </Box>
-                </Box>
                <Box sx={{
                    my: 3,
                }}>
                 <Box style={{height: "800px", width:"800px"}}>
-                    <CarouselReview />
+                    <CarouselReview reviews={reviews} />
                 </Box>
                 </Box>
             </Box>
